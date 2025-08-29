f68d1c73036b608350b8d166604ff950212d927d
sayler87 <33794015+sayler87@users.noreply.github.com>
2025-08-30 01:59:24 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from pathlib import Path
 
-st.set_page_config(page_title="AvgangListe", layout="wide")
+st.set_page_config(page_title="Registrer Avganger - Transportsystem", layout="wide")
 
-html_file = Path("registrer_avganger04.html")
+html_file = Path("app.html")
 if not html_file.exists():
-    st.error("registrer_avganger04.html")
+    st.error("app.html")
 else: 
  html_code = html_file.read_text(encoding="utf-8")
  st.components.v1.html(html_code, height=900, scrolling=True)
